0e280fdf7fc673abd02383930bd68b41521698d0
heechan <sk980919@naver.com>
2023-02-24 20:00:38 +0900

    feat: refactored email-check javascript & updated user accessibility

--- static/registration/check_email.js
+++ static/registration/check_email.js
@@ -1,18 +1,21 @@
 const emailInput = document.querySelector('#email');
 const emailError = document.querySelector('#email-duplication-check');
+let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
 
 emailInput.addEventListener('blur', () => {
     const email = emailInput.value;
-    if (email === '') {
-        emailError.textContent = '이메일을 입력해주세요.';
-    }
     fetch(`/signup/check_email_duplication/?email=${email}`)
         .then(response => response.json())
         .then(data => {
             if (data.is_exists) {
                 emailError.textContent = '이미 사용중인 이메일입니다.';
             } else {
-                emailError.textContent = '사용 가능한 이메일입니다.';
+                if (regex.test(email)) {
+                    emailError.textContent = '사용 가능한 이메일입니다.';
+                }
+                else {
+                    emailError.textContent = '올바른 이메일을 입력해주세요.';
+                }
             }
         });
     });
