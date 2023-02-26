1707ae8766161ea788d026f9b7a9cb3717964eeb
Taeho Kim <39511892+Train0303@users.noreply.github.com>
2023-02-26 20:10:13 +0900

    Pull request feature/updateSubscribe to develop (#4)
    * feat: implement subcribe update View & refactoring create subscribe
    
    - 구독한 RSS의 이름 및 학과 공지사항 링크를 수정할 수 있게 추가
    - RSS 구독 생성 에러 메세지 출력 로직 변경
    - 학과 공지사항 링크 인증시 404에러가 발생하면 에러 메세지가 출력
    
    * refac: change message clean_notice_link validation
    
    학과 링크 인증 시 404에러가 발생했을 때 출력되는 에러 메세지를 수정했습니다.
    
    * feat: add UpdateSubscribeView permission only writer
    
    다음과 같은 변경사항이 있습니다.
    - 작성자만 구독 사항을 업데이트할 수 있도록 사용자와 작성자의 권한 확인 로직을 추가했습니다.

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 
 # view import
-from .views import CreateSubscribeView
+from .views import CreateSubscribeView, UpdateSubscribeView
 
 app_name = 'subscribe'
 urlpatterns = [
-    path('', CreateSubscribeView.as_view(), name="create-subscribe")
+    path('', CreateSubscribeView.as_view(), name="create-subscribe"),
+    path('update/<int:pk>', UpdateSubscribeView.as_view(), name="update-subscribe"),
 ]
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy,reverse
+from django.views.generic import FormView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.db import transaction
 
-from .forms import CreateSubscribeForm
+from .forms import CreateSubscribeForm, UpdateSubscribeForm
 from .models import Subscribe
+from .permission import WriterPermissionRequiredMixin
 from notice.models import Notice
 
 
@@ -16,11 +19,13 @@ class CreateSubscribeView(LoginRequiredMixin, FormView):
     success_url = reverse_lazy('registration:index')
     login_url = reverse_lazy('registration:login')
 
+    @transaction.atomic()
     def form_valid(self, form):
         notice, created = Notice.objects.get_or_create(rss_link=form.cleaned_data.get("rss_link"))
+
         if Subscribe.objects.filter(notice=notice, user=self.request.user).exists():
             messages.warning(self.request, "중복된 RSS링크가 존재합니다.")
-            return redirect("subscribe:create-subscribe")
+            return render(self.request, self.template_name, {'form': form})
 
         save_data = {
             'user': self.request.user,
@@ -30,4 +35,36 @@ class CreateSubscribeView(LoginRequiredMixin, FormView):
         }
 
         form.save(save_data)
+        return redirect(self.success_url)
+
+
+class UpdateSubscribeView(WriterPermissionRequiredMixin, View):
+    template_name: str = 'subscribe/update_subscribe.html'
+    success_url: str = reverse_lazy('registration:index')
+    login_url: str = reverse_lazy('registration:login')
+    queryset = Subscribe.objects.select_related("notice")
+    check_permission_path_variable = 'pk'
+
+    def get_object(self, pk):
+        return get_object_or_404(Subscribe.objects.select_related("notice"), pk=pk)
+
+    def get(self, request, pk):
+        subscribe = self.get_object(pk)
+
+        return render(request, self.template_name, {
+            "subscribe": subscribe
+        })
+
+    def post(self, request, pk):
+        context = self.request.POST
+        form = UpdateSubscribeForm(context)
+        if not form.is_valid():
+            messages.warning(self.request, form.errors)
+            return redirect(reverse("subscribe:update-subscribe", args=(pk,)))
+
+        subscribe: Subscribe = self.get_object(pk)
+        subscribe.title = context.get('title')
+        subscribe.notice_link = context.get('notice_link')
+        subscribe.save()
+
         return redirect(self.success_url)
