dab940ed80809c74a173a8f17c1fe9274ec653b5
Taeho Kim <39511892+Train0303@users.noreply.github.com>
2023-03-05 21:29:51 +0900

    feat: implements django-mysql (#11)
    다음과 같은 추가사항이 있습니다.
    - 배포를 위한 mysql서버와 django서버간의 연동을 성공했습니다.
    - admin페이지에 접근을 위한 superuser계정을 자동으로 생성할 수 있도록 `initadmin.py` 커맨드를 추가했습니다.
    - 기타 배포를 위한 docker와 docker-compose 파일의 수정이 있습니다.

--- pnuNoti/settings/prod.py
+++ pnuNoti/settings/prod.py
@@ -1,13 +1,30 @@
 from .base import *
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 # 설정한 도메인만 가능하도록 변경
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 
+MYSQL_USER = get_secret("MYSQL_USER")
+MYSQL_PW = get_secret("MYSQL_PASSWORD")
+MYSQL_HOST = get_secret("MYSQL_HOST")
+ADMIN_EMAIL = get_secret("ADMIN_EMAIL")
+ADMIN_PW = get_secret("ADMIN_PW")
+
 # 향후 운영단계에서의 디비는 mysql로 전환할 예정
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pnu_noti',
+        'USER': MYSQL_USER,
+        'PASSWORD': MYSQL_PW,
+        'HOST': MYSQL_HOST,
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        }
+    },
 }
---
+++ registration/management/commands/initadmin.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+from registration.models import CustomUser
+
+User: CustomUser = get_user_model()
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.filter(email__iexact=settings.ADMIN_EMAIL)
+        if len(user) == 0:
+            email = settings.ADMIN_EMAIL
+            password = settings.ADMIN_PW
+            print('Creating account for %s' % (email))
+            admin = User.objects.create_superuser(email=email, password=password)
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+        else:
+            print('Admin accounts can only be initialized if no Accounts exist')
