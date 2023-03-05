e54c615bd58cce843208f001b56caf46cd3fb9bb
Kong Heechan <sk980919@naver.com>
2023-03-05 19:28:19 +0900

    feat: add favicon & userguide of subscribe (#10)

--- pnuNoti/urls.py
+++ pnuNoti/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('registration.urls')),
     path('notice/', include('notice.urls')),
     path('subscribe/', include('subscribe.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
 ]
