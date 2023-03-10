762e26de0e84505e9abec728e90ba0a691b0fecc
Taeho <rjsdnxogh55@gmail.com>
2023-03-10 20:43:32 +0900

    refac: crontab refactoring
    다음과 같은 변경사항이 있습니다.
    - 크론탭 삭제를 조금 더 쉽게 할 수 있도록 바꿨습니다.

--- pnuNoti/settings/local.py
+++ pnuNoti/settings/local.py
@@ -3,6 +3,9 @@ from .base import *
 ALLOWED_HOSTS = ["*"]
 DEBUG = True
 
+ADMIN_EMAIL = get_secret("ADMIN_EMAIL")
+ADMIN_PW = get_secret("ADMIN_PW")
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
