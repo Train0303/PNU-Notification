b7e8e22ec5fbc922ace4356102613014a341139a
heechan <sk980919@naver.com>
2023-03-11 19:41:40 +0900

    feat: implement subscribe deletion
    다음과 같은 변경사항이 있습니다.
    - 구독한 등록 내용에 대한 취소 기능을 구현하였습니다.
    - 구독 링크 등록 페이지의 RSS링크와 공지사항링크의 순서를 변경하였습니다.

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path
 
 # view import
-from .views import CreateSubscribeView, UpdateSubscribeView, UpdateSubscribeActiveView
+from .views import CreateSubscribeView, UpdateSubscribeView, UpdateSubscribeActiveView, DeleteSubscribeView
 
 app_name = 'subscribe'
 urlpatterns = [
     path('', CreateSubscribeView.as_view(), name="create-subscribe"),
     path('update/<int:pk>', UpdateSubscribeView.as_view(), name="update-subscribe"),
     path('update/active/<int:pk>', UpdateSubscribeActiveView.as_view(), name="update-subscribe-active"),
+    path('delete/<int:pk>', DeleteSubscribeView.as_view(), name="delete-subscribe")
 ]
