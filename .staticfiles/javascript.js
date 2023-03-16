8ede6cc7c9d06a528097391d37e92db6607f0e5c
heechan <sk980919@naver.com>
2023-03-16 22:53:23 +0900

    feat: modified navbar when opening

---
+++ .staticfiles/javascript.js
@@ -0,0 +1,11 @@
+// navbar click js start
+const navbar = document.querySelector('.navbar');
+
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+  if (this.getAttribute('aria-expanded') === 'true') {
+    navbar.style.marginBottom = '150px';
+  } else {
+    navbar.style.marginBottom = '0';
+  }
+});
+// navbar click js end
