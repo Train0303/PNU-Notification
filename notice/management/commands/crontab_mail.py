ee19a3985dd083e94425a2c866b289166f67b073
Taeho <rjsdnxogh55@gmail.com>
2023-03-22 17:35:14 +0900

    fix: make exception

--- notice/management/commands/crontab_mail.py
+++ notice/management/commands/crontab_mail.py
@@ -1,6 +1,5 @@
 # django import
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 from django.core.mail import send_mail
 from asgiref.sync import sync_to_async
 from django.conf import settings
@@ -30,6 +29,7 @@ def get_user_subscribes(notice: Notice):
 def reject_subscribe_email(failed_user: Set[str]):
     Subscribe.objects.filter(user__email__in=list(failed_user)).update(is_active=False)
 
+
 @sync_to_async
 def update_exec_time(notice: Notice, last_data_time: datetime):
     notice.updated_at = last_data_time
@@ -52,46 +52,68 @@ def get_message(user_subscribe, valid_item):
 작성자: {valid_item.get('author')}"""
 
 
+def send_failed_message_to_admin(failed_notices: List[Notice]):
+    send_data = f"발생 시각: {datetime.now()}\n"
+    for notice in failed_notices:
+        send_data += f"""'공지사항 ID': {notice.id}
+'공지사항 링크': {notice.rss_link}
+----------------------
+
+"""
+
+    send_mail(
+        subject='crontab_mail에서 에러가 발생했습니다.',
+        message=send_data,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[admin[1] for admin in settings.ADMIN_EMAIL])
+
+
 async def send_rss_to_user(notice: Notice):
     """
     특정 학과의 공지사항을 받아와 마지막 갱신 시간보다 뒤에 등록된 글들을 구독한 회원들에게 메일전송
     """
-    response: Response = get(url=notice.rss_link)
-    res_xml: dict = xmltodict.parse(response.text)
-    res_items: List[dict] = res_xml['rss']['channel']['item']
-    res_filter = map(lambda x: {
-        'notice_title': x['title'],
-        'link': x['link'],
-        'pubDate': datetime.strptime(x['pubDate'], '%Y-%m-%d %H:%M:%S.%f'),
-        'author': x['author']}, res_items)
+    try:
+        response: Response = get(url=notice.rss_link, timeout=5)
+        res_xml: dict = xmltodict.parse(response.text)
+        res_items: List[dict] = res_xml['rss']['channel']['item']
+        res_filter = map(lambda x: {
+            'notice_title': x['title'],
+            'link': x['link'],
+            'pubDate': datetime.strptime(x['pubDate'], '%Y-%m-%d %H:%M:%S.%f'),
+            'author': x['author']}, res_items)
 
-    valid_items = list(filter(lambda x: notice.is_valid(x['pubDate']), res_filter))
-    last_data_time = valid_items[0]["pubDate"] if valid_items else -1
-    valid_items.reverse()
+        valid_items = list(filter(lambda x: notice.is_valid(x['pubDate']), res_filter))
+        last_data_time = valid_items[0]["pubDate"] if valid_items else -1
+        valid_items.reverse()
 
-    user_subscribes: List[Subscribe] = await get_user_subscribes(notice)
+        user_subscribes: List[Subscribe] = await get_user_subscribes(notice)
 
-    failed_mail_users = set()
-    for valid_item in valid_items:
-        valid_item['pubDate'] = valid_item['pubDate'].strftime("%Y-%m-%d %H:%M")
-        tasks = []
-        for s in user_subscribes:
-            send_mail_data = {
-                'subject': f"{s.title}: {valid_item.get('notice_title')}",
-                'message': get_message(s, valid_item),
-                'from_email': settings.DEFAULT_FROM_EMAIL,
-                'recipient_list': [s.user.email],
-                'fail_silently': False
-            }
-            tasks.append(send_mail_async(send_mail_data))
+        failed_mail_users = set()
+        for valid_item in valid_items:
+            valid_item['pubDate'] = valid_item['pubDate'].strftime("%Y-%m-%d %H:%M")
+            tasks = []
+            for s in user_subscribes:
+                send_mail_data = {
+                    'subject': f"{s.title}: {valid_item.get('notice_title')}",
+                    'message': get_message(s, valid_item),
+                    'from_email': settings.DEFAULT_FROM_EMAIL,
+                    'recipient_list': [s.user.email],
+                    'fail_silently': False
+                }
+                tasks.append(send_mail_async(send_mail_data))
 
-        failed_mail_users.update([task for task in await asyncio.gather(*tasks) if task != 1])
+            failed_mail_users.update([task for task in await asyncio.gather(*tasks) if task != 1])
 
-    if failed_mail_users:
-        await reject_subscribe_email(failed_mail_users)
+        if failed_mail_users:
+            await reject_subscribe_email(failed_mail_users)
 
-    if last_data_time != -1:
-        await update_exec_time(notice, last_data_time)
+        if last_data_time != -1:
+            await update_exec_time(notice, last_data_time)
+        return -1
+    except Exception as e:
+        print(e)
+        return notice.id
+
 
 class Command(BaseCommand):
     """
@@ -101,11 +123,24 @@ class Command(BaseCommand):
     help = '공지사항 이메일 전송입니다.'
 
     def handle(self, *args: Any, **options: Any) -> NoReturn:
-        rss_datas = Notice.objects.all()[:]
+        notices = Notice.objects.all()[:]
         start_time = time.time()
         loop = asyncio.get_event_loop()
-        tasks = list(map(lambda x: asyncio.ensure_future(send_rss_to_user(x)), rss_datas))
+        results = []
+        tasks = list(map(lambda x: asyncio.ensure_future(send_rss_to_user(x)), notices))
         if tasks:
-            loop.run_until_complete(asyncio.wait(tasks))
-        # Notice.objects.all().update(updated_at=timezone.now())
+            results = loop.run_until_complete(asyncio.wait(tasks))
+
+        failed_results = set(filter(
+            lambda x: x != -1,
+            results
+        ))
+
+        if failed_results and not settings.DEBUG:
+            failed_notices = list(filter(
+                lambda x: x.id in failed_results,
+                notices
+            ))
+            send_failed_message_to_admin(failed_notices)
+
         print("Total Execute Time = ", time.time() - start_time, "s")
